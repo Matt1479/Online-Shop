999c996a5b87b5d7ec7809aedca4a56dde94e1d4
Matt1479 <gitmatt1479@gmail.com>
2023-08-18 17:23:39 +0200

    fix: Use proper select for quantity button and dropdown

--- Project/static/item.js
+++ Project/static/item.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
-    let dropdown = document.querySelector('button.dropdown-toggle');
-    document.querySelector('ul.dropdown-menu').addEventListener('click', (event) => {
+    let dropdown = document.querySelector('button#qtyBtn');
+    document.querySelector('ul#qtyDropdown').addEventListener('click', (event) => {
         dropdown.innerHTML = event.target.innerHTML;
         document.querySelector('input[name="qty"]').value = dropdown.innerHTML;
     });
